980d5fb7422ea5e43e1dff2d12d6d943c46e46b7
Ben Watkins <hi@bwatkins.dev>
2023-07-26 23:27:39 -0500

    `prettier --write .`

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,17 +4,17 @@
  * For more information, see https://remix.run/file-conventions/entry.server
  */
 
-import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
-import { RemixServer } from "@remix-run/react";
-import isbot from "isbot";
-import { renderToReadableStream } from "react-dom/server";
+import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare';
+import { RemixServer } from '@remix-run/react';
+import isbot from 'isbot';
+import { renderToReadableStream } from 'react-dom/server';
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
-  loadContext: AppLoadContext
+  loadContext: AppLoadContext,
 ) {
   const body = await renderToReadableStream(
     <RemixServer context={remixContext} url={request.url} />,
@@ -25,14 +25,14 @@ export default async function handleRequest(
         console.error(error);
         responseStatusCode = 500;
       },
-    }
+    },
   );
 
-  if (isbot(request.headers.get("user-agent"))) {
+  if (isbot(request.headers.get('user-agent'))) {
     await body.allReady;
   }
 
-  responseHeaders.set("Content-Type", "text/html");
+  responseHeaders.set('Content-Type', 'text/html');
   return new Response(body, {
     headers: responseHeaders,
     status: responseStatusCode,
--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
-import { logDevReady } from "@remix-run/cloudflare";
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-import * as build from "@remix-run/dev/server-build";
+import { logDevReady } from '@remix-run/cloudflare';
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
+import * as build from '@remix-run/dev/server-build';
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   logDevReady(build);
 }
 
